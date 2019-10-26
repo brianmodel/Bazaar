c66562aab51e2c4ff9523da910ccbb5249df2f41
brianmodel <brian.model@gmail.com>
2019-10-26 11:33:16 -0400

    Added webhook routes

--- webhook/routes.py
+++ webhook/routes.py
@@ -1,8 +1,32 @@
-from flask import request
+import json
+from flask import request, abort
 
 from webhook import app
 
 
 @app.route("/webhook", methods=["POST"])
+def webhook():
+    body = request.get_json()
+    if body["object"] == "page":
+        for entry in body["entry"]:
+            webhook_event = entry["messaging"][0]
+            print(webhook_event)
+        return "EVENT_RECIEVED"
+    else:
+        abort(404)
+
+
+@app.route("/webhook", methods=["GET"])
 def verify_webhook():
-    pass
+    VERIFY_TOKEN = "token"
+    mode = request.args.get("hub.mode", None)
+    token = request.args.get("hub.verify_token", None)
+    challenge = request.args.get("hub.challenge", None)
+
+    if mode and token:
+        if mode == "subscribe" and token == VERIFY_TOKEN:
+            print("WEBHOOK_VERIFIED")
+            return challenge
+        else:
+            abort(403)
+
