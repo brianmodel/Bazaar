0cccb9d461899f9692c324fd212adcc2f0bc4acf
Rahul Bhethanabotla <agentrmb@gmail.com>
2019-10-26 17:13:06 -0400

    ncr post/get requests

---
+++ app/ncr/order_api_tools.py
@@ -0,0 +1,110 @@
+import requests
+import json
+
+url = "https://gateway-staging.ncrcloud.com/order/orders"
+headers = {
+    'nep-correlation-id': "12345",
+    'nep-organization': "hack-bazaar",
+    'nep-enterprise-unit': "c08a3e2841f94543935c3c0db0f0723b",
+    'nep-application-key': "8a0189a96ddb1163016e066cb10c0036",
+    'Content-Type': "application/json",
+    'Authorization': "Basic YWNjdDpyb290QGhhY2tfYmF6YWFyOkc2QHp7W3EwJlo=",
+    'User-Agent': "PostmanRuntime/7.19.0",
+    'Accept': "*/*",
+    'Cache-Control': "no-cache",
+    'Postman-Token': "9fceb862-a2d5-43cb-8cc4-8e1d85ec8153,244945fe-13a7-4f11-b8e1-72c9a9d28031",
+    'Host': "gateway-staging.ncrcloud.com",
+    'Accept-Encoding': "gzip, deflate",
+    'Content-Length': "592",
+    'Cookie': "9e59b60fc083da305b706811d8f0d8f4=500eeee38cf8bfe546bf6c1991db68b4",
+    'Connection': "keep-alive",
+    'cache-control': "no-cache"
+    }
+    
+def get_generic_payload():
+    return {
+    "fulfillment": {
+        "pickupDate": "2019-10-27T21:03:46.514Z",
+        "pickupLocation": "Barry's House",
+        "fulfillmentTime": "2019-10-27T21:03:46.514Z"
+    },
+    "orderLines": [
+        {
+            "productId": {
+                "type": "n/a"
+            },
+            "quantity": {
+                "value": 0.0
+            },
+            "unitPrice": 0.0
+        }
+    ],
+    "payments": [
+        {
+            "amount": 0.0
+        }
+    ],
+    "customer": {
+        "id": "n/a",
+        "name": "n/a",
+        "firstName": "n/a",
+        "lastName": "n/a",
+        "phone": "n/a"
+    }
+}
+
+def generate_payload(**kwargs):
+    # print(properties)
+    payload = get_generic_payload()
+    update_dict(payload["fulfillment"], kwargs)
+    for properties in payload["orderLines"]:
+        update_dict(properties, kwargs)
+
+    update_dict(payload["orderLines"][0]["productId"], kwargs)
+    update_dict(payload["orderLines"][0]["quantity"], kwargs)
+    update_dict(payload["payments"][0], kwargs)
+    update_dict(payload["customer"], kwargs)
+    print(payload)
+    return json.dumps(payload)
+
+def update_dict(properties, new_properties):
+    for key in properties.keys():
+        if key in new_properties and type(properties[key]) == type(new_properties[key]):
+            print(key)
+            properties[key] = new_properties[key]
+
+def create_order(**kwargs):
+    # print(generate_payload(kwargs))
+    print("KWARGS", kwargs)
+    response = requests.request("POST", url, data=generate_payload(**kwargs), headers=headers)
+    print(response.status_code)
+    print(response.content)
+
+# create_order(name="test", value="sfsadf")
+
+
+
+def get_order(id):
+    newheaders = {
+        'nep-correlation-id': "12345",
+        'nep-application-key': "8a0189a96ddb1163016e066cb10c0036",
+        'nep-organization': "hack-bazaar",
+        'Content-Type': "application/json",
+        'Authorization': "Basic YWNjdDpyb290QGhhY2tfYmF6YWFyOkc2QHp7W3EwJlo=",
+        'User-Agent': "PostmanRuntime/7.19.0",
+        'Accept': "*/*",
+        'Cache-Control': "no-cache",
+        'Postman-Token': "2e2b69c9-1c8e-47be-b909-3b4bbdba0677,0b247a16-2103-4d4c-8b6c-a17ce933bf86",
+        'Host': "gateway-staging.ncrcloud.com",
+        'Accept-Encoding': "gzip, deflate",
+        'Cookie': "9e59b60fc083da305b706811d8f0d8f4=500eeee38cf8bfe546bf6c1991db68b4",
+        'Connection': "keep-alive",
+        'cache-control': "no-cache"
+    }
+
+    newurl = url + "/" + str(id)
+    response = requests.request("GET", newurl, headers=newheaders)
+    print(response.status_code)
+    print(response.content)
+
+# get_order(13108171952785862744)
