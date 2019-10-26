5c8c922f221675a6977a0f3d699fdd07ef2c1847
brianmodel <brian.model@gmail.com>
2019-10-26 13:13:47 -0400

    fixed typos

--- webhook/handlers.py
+++ webhook/handlers.py
@@ -1,15 +1,14 @@
 import json
-import request
+import requests
 
 from webhook.settings import SETTINGS
+
 
 def handle_message(sender_psid, received_message):
     response = {}
     print(received_message)
-    if 'text' in received_message:
-        response = {
-            'text': received_message['text']
-        }
+    if "text" in received_message:
+        response = {"text": received_message["text"]}
     call_send_api(sender_psid, response)
 
 
@@ -18,15 +17,8 @@ def handle_postback(sender_psid, received_postback):
 
 
 def call_send_api(sender_psid, response):
-    request_body = {
-        'recipient': {
-            'id': sender_psid
-        }
-        'message': response
-    }
-    uri = 'https://graph.facebook.com/v2.6/me/messages'
-    params = {
-        'access_token': SETTINGS['PAGE_ACCESS_TOKEN']
-    }
+    request_body = {"recipient": {"id": sender_psid}, "message": response}
+    uri = "https://graph.facebook.com/v2.6/me/messages"
+    params = {"access_token": SETTINGS["PAGE_ACCESS_TOKEN"]}
     response = request.post(uri, data=request_body, params=params)
     print(response)
